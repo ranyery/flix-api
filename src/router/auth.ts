c593ba7f6023031f0f23fdf7ad8872a9e7632cb2
ranyery <ranyerysc@gmail.com>
2023-04-09 15:53:33 -0300

    updated auth endpoint

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -10,7 +10,7 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 
 // https://www.youtube.com/watch?v=b8ZUb_Okxro
 export default (router: Router) => {
-  router.post("/users/signup", create);
+  router.post("/auth/signup", create);
   router.post("/auth/login", authenticate);
   router.post("/auth/refresh-token", authMiddleware, updateToken);
   router.post("/auth/validate-token", authMiddleware, validateToken);
