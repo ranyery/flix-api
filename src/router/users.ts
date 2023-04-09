e5922120682412dd03c2a08ba8de2580a3e37cf7
ranyery <ranyerysc@gmail.com>
2023-04-09 08:52:11 -0300

    chore: youtube video to remember

--- src/router/users.ts
+++ src/router/users.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticate, updateToken, validateToken } from "../controllers/users";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
+// https://www.youtube.com/watch?v=b8ZUb_Okxro
 export default (router: Router) => {
   // router.post("/users/signup", create);
   router.post("/users/login", authenticate);
